564ee1e2aa146e380171a56af2c44b64861ee1d9
Hamxah72 <154810847+Hamxah72@users.noreply.github.com>
2024-01-12 21:49:10 +0100

    Add files via upload

---
+++ vars.py
@@ -0,0 +1,20 @@
+# print("\nThis numbers calculates the average of two numbers")
+number1 = 10525
+number2 = 13534543
+# print("The numbers are", number1, "and", number2)
+# print("The average is: ", (number1 + number2) / 2 )
+
+# # input funtions 
+
+# number1 = input("Please enter the first number: ") 
+# print(number1)
+# number1 = input("Please enter the second number: ") 
+# print(number1)
+# print(type(number1))
+# print(number1 + number2)
+
+first_name = "Ishaq"
+last_name = "Hamza"
+print("your email address is", first_name + "" + last_name + "@gmail.com")
+
+print(float(number1) + float(number2))
